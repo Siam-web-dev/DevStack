21723d6fac86e3e0887164e1c6e57ca56dca981b
Kamruzzaman Siam <ksiam9193@gmail.com>
2026-09-21 12:56:09 +0600

    Responsive footer and nav sticky fixed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,26 +5,26 @@ const Footer = () => {
     <div className="max-w-[90%] sm:max-w-[90%] mx-auto mt-35 ">
       {/* Footer Top */}
       <div className="flex justify-between">
-        <div className="space-y-5">
-            <img src={Logo} alt="" />
-            <p className="text-[#64748B] text-sm">Curated tools, technologies, and resources for developers building <br /> modern software.</p>
+        <div className="space-y-5 flex flex-col items-center sm:items-start">
+            <img src={Logo} alt=""  />
+            <p className="text-[#64748B] text-center sm:text-start text-sm">Curated tools, technologies, and resources for developers building <br /> modern software.</p>
             <div className="flex gap-4 text-[#475569]">
                 <a href="#">GitHub</a><a href="#">Twitter</a><a href="#">LinkedIn</a>
             </div>
         </div>
-        <div className=" list-none space-y-2 text-[#64748B] " >
+        <div className=" hidden sm:block list-none space-y-2 text-[#64748B] " >
             <h3 className="font-semibold text-black">PRODUCT</h3>
             <li><a href="">Home</a></li>
             <li><a href="">Technologies</a></li>
             <li><a href="">Projects</a></li>
         </div>
-        <div className=" list-none space-y-2 text-[#64748B] ">
+        <div className=" hidden sm:block list-none space-y-2 text-[#64748B] ">
             <h3 className="font-semibold text-black">COMPANY</h3>
             <li><a href="">About</a></li>
             <li><a href="">Contact </a></li>
             <li><a href="">Careers</a></li>
         </div>
-        <div className="list-none space-y-2 text-[#64748B]">
+        <div className=" hidden sm:block list-none space-y-2 text-[#64748B]">
             <h3 className="font-semibold text-black">LEGAL</h3>
             <li><a href="">Privacy Policy</a></li>
             <li><a href="">Terms of Service</a></li>
@@ -35,8 +35,8 @@ const Footer = () => {
       <div className="my-5 border-t border-slate-100"></div>
       {/* Footer bottom */}
       <div className="flex justify-between my-12">
-            <p className="text-[#64748B]">© 2026 Dev Stack. All rights reserved.</p>
-            <div className="flex gap-4 text-[#64748B]">
+            <p className="text-[#64748B] text-xs  sm:text-sm ">© 2026 Dev Stack. All rights reserved.</p>
+            <div className="flex gap-4 text-[#64748B] text-xs  sm:text-sm">
                 <a href="#">Privacy</a>
                 <a href="#">Terms</a>
             </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Logo from "../assets/logo-text.png"
 import hamburger from "../assets/hamburger.png"
 const Navbar = () => {
     return (
-        <div className="flex justify-between items-center py-6 max-w-[95%] sm:max-w-[90%] mx-auto sticky top-0 z-10">
+        <div className="flex justify-between items-center py-6 max-w-[95%] sm:max-w-[90%] mx-auto sticky top-0 z-10 bg-white">
             <img src={hamburger} alt="" className=" sm:hidden " />
             <img src={Logo} alt="" className=" h-5 w-auto sm:h-10 sm:w-auto " />
             <ul className=" hidden md:flex gap-6 text-[#DB2777]">
